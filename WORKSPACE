1697c0d954a90500b2c5defadcb1e5ce84a6622d
Aswin Mylraj <amylraj@endor.ai>
2024-09-18 14:30:01 +0530

    Using HTTP Jar for Google Guava Test Flow

--- WORKSPACE
+++ WORKSPACE
@@ -1,1 +1,13 @@
-# Marker file that this is the root of a Bazel workspace.
+load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_jar")
+
+http_jar(
+    name = "mvn_google_guava",
+    sha256 = "a42edc9cab792e39fe39bb94f3fca655ed157ff87a8af78e1d6ba5b07c4a00ab",
+    url = "https://repo1.maven.org/maven2/com/google/guava/guava/31.1-jre/guava-31.1-jre.jar",
+)
+
+http_jar(
+    name = "mvn_google_guava_failureaccess",  # A Transitive Dep of com.google.guava:guava:31.1-jre
+    sha256 = "8a8f81cf9b359e3f6dfa691a1e776985c061ef2f223c9b2c80753e1b458e8064",
+    url = "https://repo1.maven.org/maven2/com/google/guava/failureaccess/1.0.2/failureaccess-1.0.2.jar",
+)
---
+++ src/main/java/com/example/myproject/ClassWithTransitiveImports.java
@@ -0,0 +1,33 @@
+package com.example.myproject;
+
+import com.google.common.math.BigDecimalMath;
+import com.google.common.util.concurrent.ListenableFutureTask;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ClassWithTransitiveImports {
+
+    private ListenableFutureTask<Double> getListenableTask(int time) { // Has Transitives
+        return ListenableFutureTask.create(() -> {
+            BigDecimal a = BigDecimal.valueOf(Math.random());
+            BigDecimal b = BigDecimal.valueOf(Math.random());
+
+            Thread.sleep(time * 1000);
+            System.out.println("SomeOutput " + BigDecimalMath.roundToDouble(a.add(b), RoundingMode.HALF_UP));
+            return BigDecimalMath.roundToDouble(a.add(b), RoundingMode.HALF_UP);
+        });
+    }
+
+    public void useSomeRandomInternalLibrary() {
+        System.out.println("Using Some Internal Function with Transitive Dependencies");
+        ExecutorService execService = Executors.newSingleThreadExecutor();
+        ListeningExecutorService lExecService = MoreExecutors.listeningDecorator(execService); // Has Transitives
+
+        lExecService.submit(getListenableTask(5));
+    }
+}
